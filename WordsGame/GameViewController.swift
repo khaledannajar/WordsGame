05ed43e5d1ccb8db0020605c4e11552ec900e0fa
Khaled Annajar <khaledannajar@gmail.com>
2019-06-24 01:25:03 +0300

    rename ViewController to be GameViewController, draw game view

---
+++ WordsGame/GameViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  WordsGame
+//
+//  Created by khaledannajar on 6/21/19.
+//  Copyright © 2019 khaledannajar. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    @IBOutlet weak var spaWordLabel: UILabel!
+    @IBOutlet weak var enWordLabel: UILabel!
+    @IBOutlet weak var rightButton: UIButton!
+    @IBOutlet weak var wrongButton: UIButton!
+    
+    @IBOutlet weak var scoreValueLabel: UILabel!
+    @IBOutlet weak var endLineView: UIView!
+    
+    @IBOutlet weak var wordLabelTopConstraint: NSLayoutConstraint!
+    private let wordLabelTopConstraintDefaultValue: CGFloat = 10
+    private var wordLabelMaxTopConstraintValue: CGFloat = 200
+    let repo = WordsRepository()
+    
+    var tappedAbutton = false
+    let timeBetweenWords = 3.0
+    var timeWordShown = 0.0
+    var currentWordIndex = 0
+    var currentWord: Word?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupButtons()
+        fetchWords()
+    }
+    
+    func setupButtons() {
+        rightButton.layer.cornerRadius = 5
+        wrongButton.layer.cornerRadius = 5
+    }
+    
+    func fetchWords() {
+        repo.getWords(refresh: true) { (words, error) in
+            if error != nil {
+                print(error!)
+            }
+            if let words = words {
+                print("words count = \(words.count)")
+                
+                DispatchQueue.main.async {
+                    self.show()
+                }
+            }
+        }
+    }
+    
+    func show() {
+        
+    }
+    
+    @IBAction func wrongButtonTapped(_ sender: Any) {
+        
+    }
+    @IBAction func rightButtonTapped(_ sender: Any) {
+        
+    }
+    
+    
+}
+
